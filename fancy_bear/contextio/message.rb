bf52bc3a520bff96be32cfb815738660a607f3ab
Teddy Wing <git@teddywing.com>
2014-06-08 12:19:48 -0400

    Create JSON route for message list
    Create a route to get a list of messages in JSON, tied to a hard-coded
    email account for now.

---
+++ fancy_bear/contextio/message.rb
@@ -0,0 +1,33 @@
+require 'contextio'
+
+module FancyBear
+  module ContextIO
+    
+    class Message < Base
+      def initialize(email)
+        super()
+        
+        @account = @contextio.accounts.where(:email => email).first
+      end
+      
+      def all
+        messages = []
+        @account.messages.each do |m|
+          messages << {
+            :id => m.message_id,
+            :from_address => m.from['email'],
+            :from_name => m.from['name'],
+            :subject => m.subject,
+            :timestamp => m.date
+          }
+        end
+        messages
+      end
+      
+      def get
+        
+      end
+    end
+    
+  end
+end
